a12f8ec26d7873be8d00d10c034ade4916759567
Nomanie <lukaszbielecki7@gmail.com>
2021-03-16 09:17:31 +0100

    qwe

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using AdressBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdressBook.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public Address Address { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+
+                Name = "User";
+
+            }
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+
+    }
+}
